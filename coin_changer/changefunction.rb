097b8177d5c8cdee8bcc7c346f2ca365bc2b7e29
Michael Moore <Michael Moore>
2016-09-13 16:21:52 -0400

    Added hash code for 1 penny.

---
+++ coin_changer/changefunction.rb
@@ -0,0 +1,8 @@
+def coin_changer(cents_received)
+	coins = {}
+	if cents_received == 1
+		coins[:penny] = 1
+	end
+	coins
+end
+
---
+++ coin_changer/coin_changer.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require_relative "changefunction.rb"
+
+class TestChangeFunction < Minitest::Test
+
+	def test_0_cents_returns_empty_hash
+		cents_received = 0
+		assert_equal({}, coin_changer(cents_received))
+	end
+
+	def test_one_cent_is_one_penny
+		cents_received = 1
+		assert_equal({:penny => 1}, coin_changer(cents_received))
+	end
+
+	# def test_six_cents_equals_one_nickel_and_one_penny
+	# 	cents_received = 6
+	# 	assert_equal({:penny => 1, nickel => 1})
+
+end
+
