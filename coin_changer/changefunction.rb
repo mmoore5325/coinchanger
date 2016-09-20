cca0050f571756856f3bbedb7121c3e5929f3fd7
Michael Moore <Michael Moore>
2016-09-20 11:04:02 -0400

    Nickels and pennies

--- coin_changer/changefunction.rb
+++ coin_changer/changefunction.rb
@@ -6,16 +6,27 @@ def coin_changer(cents)
 		# 	cents_received = cents_received % 10
 		# end
 
-
 	if cents >= 5
-		coins[:nickel] = cents / 5
+		coins[:nickle] = cents / 5
 		cents = cents % 5
+	end 
+	
+	if cents >= 1
+		coins[:penny] = cents
+	end
+		coins
 	end
 
-	if cents >= 1
-		coins[:penny] = cents / 1 
-		cents = cents % 1     
-	end
-	coins
-end
+
+# 	if cents >= 5
+# 		coins[:nickel] = cents / 5
+# 		cents = cents % 5
+# 	end
+
+# 	if cents >= 1
+# 		coins[:penny] = cents / 1 
+# 		cents = cents % 1     
+# 	end
+# 	coins
+# end
 
--- coin_changer/coin_changer.rb
+++ coin_changer/coin_changer.rb
@@ -8,20 +8,17 @@ class TestCoinChanger < Minitest::Test
 	end
 
 	def test_1_cent_equals_1_penny
-		cents = 1
-		assert_equal({:penny => 1}, coin_changer(cents))
+		assert_equal({:penny => 1}, coin_changer(1))
 	end
 
 	def test_two_cents_returns_two_pennys
-		cents = 2
-		assert_equal({:penny => 2}, coin_changer(cents))
+		assert_equal({:penny => 2}, coin_changer(2))
 	end
 
 	def test_five_cents_returns_one_nickle
-		cents = 5
-		assert_equal({:nickle => 1}, coin_changer(cents))
+		assert_equal({:nickle => 1}, coin_changer(5))
 	end
-
+	
 	# def test_six_cents_returns_one_and_one
 	# 	cents = 6
 	# 	assert_equal({:nickle => 1, :penny =>1}, coin_changer(cents))
